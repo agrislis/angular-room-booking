ab7e2945522484763fb2f4bca826b207c8ea9116
murmurssa <alissamurmurss@gmail.com>
2020-02-26 18:19:42 +0300

    fix form validation HW-4

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -13,15 +13,21 @@ export class FormComponent implements OnInit {
   room2: Event[];
   room3: Event[];
 
+  currentTime = new Date();
+
+  isBusy: boolean;
+
+  formDirective: FormGroupDirective;
+
   eventForm = new FormGroup({
     title: new FormControl('', [
       Validators.required,
       Validators.pattern(/[A-zА-я0-9,]/)
     ]),
-    date: new FormControl('', Validators.required),
+    date: new FormControl(this.currentTime),
     timeStart: new FormControl('', Validators.required),
     timeEnd: new FormControl('', Validators.required),
-    roomId: new FormControl('1', Validators.required)
+    roomId: new FormControl('1')
   });
 
   constructor(private apiservice: ApiService) { }
@@ -30,15 +36,25 @@ export class FormComponent implements OnInit {
     this.getEvent();
   }
 
-  onSubmit(): void {
-    this.apiservice.addEvent({
-      roomId: this.eventForm.controls.roomId.value,
-      title: this.eventForm.controls.title.value,
-      date: this.eventForm.controls.date.value,
-      timeStart: this.eventForm.controls.timeStart.value,
-      timeEnd: this.eventForm.controls.timeEnd.value,
-    });
-    this.getEvent();
+  onSubmit(formDirective: FormGroupDirective): void {
+    switch (this.eventForm.controls.roomId.value) {
+      case '1': {
+        this.checkTimeSlot(this.room1);
+        break;
+      }
+      case '2': {
+        this.checkTimeSlot(this.room2);
+        break;
+      }
+      case '3': {
+        this.checkTimeSlot(this.room3);
+        break;
+      }
+    }
+
+    if (this.isBusy === false) {
+      this.resetForm(formDirective);
+    }
   }
 
   getEvent(): void {
@@ -53,9 +69,40 @@ export class FormComponent implements OnInit {
     formDirective.resetForm();
     this.eventForm.reset({
       title: '',
-      date: '',
+      date: this.currentTime,
       timeStart: '',
+      timeEnd: '',
       roomId: '1'
     });
+  }
+
+  addEvent(): void {
+    this.apiservice.addEvent({
+      roomId: this.eventForm.controls.roomId.value,
+      title: this.eventForm.controls.title.value,
+      date: this.eventForm.controls.date.value,
+      timeStart: this.eventForm.controls.timeStart.value,
+      timeEnd: this.eventForm.controls.timeEnd.value,
+    });
+    this.getEvent();
+  }
+
+  checkTimeSlot(arrayOfEvents: Event[]): boolean {
+    let setTimeStart = this.eventForm.controls.timeStart.value;
+    let setTimeEnd = this.eventForm.controls.timeEnd.value;
+    let busySlot = 0;
+
+    arrayOfEvents.forEach((event) => {
+      if ((setTimeStart >= event['timeStart'] && setTimeStart <= event['timeEnd']) || (setTimeEnd >= event['timeStart'] && setTimeEnd <= event['timeEnd'])) {
+        ++busySlot;
+      }
+    });
+
+    if (busySlot > 0) {
+      return this.isBusy = true;
+    } else {
+      this.addEvent();
+      return this.isBusy = false;
+    }
   }
 }
